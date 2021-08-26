2252451e64d926670e6523da0e80e45a8ac83761
Patch Osodo <osodopatch@yahoo.com>
2021-08-26 00:42:00 -0700

    Print to standard error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry point
+ *
+ *Return: 1
+ */
+int main(void)
+{
+fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59, 1, stderr);
+return(1);
+}
