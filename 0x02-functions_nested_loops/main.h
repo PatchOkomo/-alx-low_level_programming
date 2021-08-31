c3d0d371d1c2e96683c2165dc8e62bbddb8e9809
Patch Osodo <osodopatch@yahoo.com>
2021-08-31 04:58:11 -0700

    Printing to 98 using printf

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -62,4 +62,9 @@ void times_table(void);
  *int add(int, int) adds two integers and returns the result
  */
 int add(int, int);
+
+/**
+ *void print_to_98(int n) prints natural numbers from n to 98;
+ */
+void print_to_98(int n);
 #endif
