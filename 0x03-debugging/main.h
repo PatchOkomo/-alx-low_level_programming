7397cabb237e703371957774740fa3d9619038b1
Patch Osodo <osodopatch@yahoo.com>
2021-08-31 23:06:07 -0700

    The largest number of 3

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -3,6 +3,13 @@
 
 #include <stdio.h>
 
+/**
+ * positive_or_negative checks whether i is positive or negative
+ */
 void positive_or_negative(int i);
 
+/**
+ *int largest_number returns the largest number of three
+ */
+int largest_number(int a, int b, int c);
 #endif /* MAIN_H */
