11432762ee9c711e907ac332dc312076f7d7e042
Patch Osodo <osodopatch@yahoo.com>
2021-09-02 06:16:17 -0700

    Debugging to understand code

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -7,19 +7,19 @@
 
 int main(void)
 {
-	unsigned long number = 612852475143;
-	unsigned long prime = 2;
+	unsigned long number = 612852475143;/*Variable for the large number*/
+	unsigned long x = 2;/*divisor to check for the largest prime*/
 
-	while (prime < number)
+	while (x < number)/*Ensuring the factor does not exceed the number*/
 	{
-		if (number % prime == 0)
+		if (number % x == 0)
 		{
-			number /= prime;
-			prime = 2;
+			number /= x;/*division to get next factor*/
+			x = 2;/**/
 		}
 		else
 		{
-			prime++;
+			x++;/*Increasing value to get to next divisor*/
 		}
 	}
 	printf("%lu\n", number);
