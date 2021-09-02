21cf40edda02cb48984ec622e3acdaecc5ff006b
Patch Osodo <osodopatch@yahoo.com>
2021-09-02 03:17:46 -0700

    Fizz Buzz test 2

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -16,16 +16,19 @@ int main(void)
 	for (i = 1; i <= 100; i++)
 	{
 		if ((i % x) == 0)
-			printf("Fizz ");
+			printf("Fizz");
 
 		else if ((i % y) == 0)
-			printf("Buzz ");
+			printf("Buzz");
 
 		else if (((i % x) == 0) && ((i % y) == 0))
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 
 		else
 			printf("%d ", i);
+
+		if (i < 100)
+			printf(" ");
 	}
 	printf("\n");
 
