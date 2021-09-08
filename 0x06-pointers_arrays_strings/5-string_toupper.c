81837cef6798969cea830ec120a4868c5a7aa8bb
Patch Osodo <osodopatch@yahoo.com>
2021-09-08 05:10:34 -0700

    Convert to uppercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * string_toupper - converts lowercase characters to uppercase characters
+ * @c: characters to be converted
+ * Return: c(Uppercase characters)
+ */
+
+char *string_toupper(char *c)
+{
+	int i;
+	for (i = 0; c[i] != '\0'; i++)/*omits the null terminator if c exists*/
+	{
+		if (c[i] >= 'a' && c[i] <= 'z')
+		{
+			c[i] = c[i] - 'a' + 'A';
+		}
+	}
+	return (c);
+}
