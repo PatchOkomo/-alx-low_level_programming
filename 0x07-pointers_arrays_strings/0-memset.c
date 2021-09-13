31dc0e8e264dc676fc0b28fedfd2a1c0141cce3a
Patch Osodo <osodopatch@yahoo.com>
2021-09-13 00:59:45 -0700

    Set first n elements of variiable at s to 'b'

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _memset - fills the first n bytes of memory area pointed to by s with b
+ * @s: memory area to be filled
+ * @b: constant bytes to fill up the memory spaces
+ * @n: number of bytes to be filled
+ * Return: pointer to memory area 's'
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	int size = n;
+
+	if (size > 0)
+	{
+		int count;
+
+		for (count = 0; count < size; count++)
+			s[count] = b;
+	}
+
+	return(s);
+}
