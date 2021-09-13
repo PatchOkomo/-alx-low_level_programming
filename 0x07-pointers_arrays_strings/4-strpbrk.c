6b4bb6d301341733df8f17c32d663f8834d87700
Patch Osodo <osodopatch@yahoo.com>
2021-09-13 10:15:24 -0700

    Corrected number of lines exceeded

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -2,8 +2,8 @@
 #define NULL 0
 
 /**
- * _strpbrk -locates the first occurrence in the string s of any of the bytes in
- *  the string accept
+ * _strpbrk - locates the first occurrence in the string s of any of the
+ *  bytes in the string accept
  * @s: string to search
  * @accept: target matches
  * Return: Pointer to ndex of first occurrence
