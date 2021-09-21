56e7c457c8b5b4c1309cba787a0e80d3bb640dcd
Patch Osodo <osodopatch@yahoo.com>
2021-09-21 04:30:04 -0700

    Initial commit, seperates words from a string

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,44 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * argstostr - seperate words from a string
+ * @ac: argument counter
+ * @av: arguments 2D array
+ * Return: new string
+ */
+char *argstostr(int ac, char **av)
+{
+	char *str;
+	int row;
+	int col;
+	int c;
+	int i = 0;
+
+	if (ac == 0 || av == NULL)
+	{
+		return (NULL);
+	}
+	for (row = 0; row < ac; row++)
+	{
+		for (col = 0; av[row][col] != '\0'; col++)
+		{
+			c++;
+		}
+	}
+	str = malloc(sizeof(char) * (c + ac + 1));
+	if (str == NULL)
+		return (NULL);
+	for (row = 0; row < ac; row++)
+	{
+		for (col = 0; av[row][col] != '\0'; col++)
+		{
+			str[i] = av[row][col];
+			i++;
+		}
+		str[i] = '\n';
+		i++;
+	}
+	return (str);
+
+}
