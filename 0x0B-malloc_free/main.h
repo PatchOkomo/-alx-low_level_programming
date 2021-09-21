1f3ef1933748f373a8d2dc49a9ca177c455cde87
Patch Osodo <osodopatch@yahoo.com>
2021-09-21 12:02:22 -0700

    Updated main.h file

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -2,18 +2,20 @@
 #define _MAIN_H_
 
 int _putchar(char c);
-
+/*0*/
 char *create_array(unsigned int size, char c);
-
+/*1*/
 char *_strdup(char *str);
-
+/*2*/
 char *str_concat(char *s1, char *s2);
-
+/*3*/
 int **alloc_grid(int width, int height);
-
+/*4*/
 void free_grid(int **grid, int height);
-
+/*100*/
 char *argstostr(int ac, char **av);
-
+/*101*/
+int word_len(char *str);
+int count_words(char *str);
 char **strtow(char *str);
 #endif/*_MAIN_H_*/
